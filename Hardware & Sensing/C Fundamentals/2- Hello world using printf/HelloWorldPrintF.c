5d109368683b67395789260fb97c7540880ab073
Shawani <121223325+TrulyAstonishin@users.noreply.github.com>
2026-02-16 21:45:20 +0300

    Hello World using Printf for C

---
+++ Hardware & Sensing/C Fundamentals/2- Hello world using printf/HelloWorldPrintF.c
@@ -0,0 +1,9 @@
+//
+// Created by Shawani on 2/16/2026.
+//
+
+#include <stdio.h>
+int main() {
+    printf("Hello World");
+    return 0;
+}
---
+++ Hardware & Sensing/C Fundamentals/2- Hello world using printf/Number.c
@@ -0,0 +1,10 @@
+//
+// Created by Shawani on 2/16/2026.
+//
+
+#include <stdio.h>
+int main() {
+    int num =42;
+    printf("The number is: %d", num);
+    return 0;
+}
